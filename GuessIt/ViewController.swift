2df88946777e7803917acda425fe1fb1c7db9ef1
Taylor Hutchison <seanthutchison@gmail.com>
2016-02-25 17:11:39 -0600

    Added random number generation and stepper functionality
    A secret number is now generated using arc4random_uniform and the
    stepper control changes the range used to generate the random number.

--- GuessIt/ViewController.swift
+++ GuessIt/ViewController.swift
@@ -9,17 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var maxGuessValue: Int = 0;
+    var secretNumber: Int = 0;
 
     @IBOutlet weak var messageToPlayer: UILabel!
     
     @IBOutlet weak var guessRangeLabel: UILabel!
     
     @IBAction func guessInput(sender: AnyObject) {
-        //let x = sender as! UITextField
+        let x = sender as! UITextField
+        messageToPlayer.text = x.text;
+        print("it works")
+    }
+    
+    @IBAction func StepperValueChanged(sender: UIStepper) {
+        self.generateSecretNumber()
+        maxGuessValue = Int(pow(Double(10),Double(sender.value)))
+        self.setGuessLabel()
+    }
+    
+    func setGuessLabel(){
+        self.guessRangeLabel.text = "1 and " + String(maxGuessValue)
+    }
+    
+    func generateSecretNumber(){
+        self.secretNumber = Int(arc4random_uniform(UInt32(maxGuessValue)) + 1)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.maxGuessValue = 10
+        self.setGuessLabel()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
